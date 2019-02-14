7098fe30cc1ed738415ae6b073f5543eea54529b
Lucas Bertin <lucas.bertin@lambda3.com.br>
2019-02-14 18:02:35 -0200

    Corrige busca de usuário logado

--- app/nivel/3/home/routes.js
+++ app/nivel/3/home/routes.js
@@ -5,15 +5,17 @@ const views = 'nivel/3/home/views'
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const configuracao = require('../config')
-const Usuario = require('../usuario/Usuario')
+const criarUsuario = require('../usuario/Usuario')
 
 module.exports = new Router({ prefix: '/nivel/3' })
   .get('/', async ctx => {
     try {
-      if(!ctx.cookies.AUTHZ)
+      const authz = ctx.cookies.get('AUTHZ')
+      const Usuario = criarUsuario(ctx.db)
+      if(!authz)
         return ctx.render(`${views}/index.hbs`, { logado: false })
 
-      const id = jwt.verify(ctx.cookies.AUTHZ, configuracao.segredo)
+      const id = jwt.verify(authz, configuracao.segredo)
       const usuario = await Usuario.findById(id)
 
       return ctx.render(`${views}/index.hbs`, { logado: !!usuario })
